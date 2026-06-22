91d80f2ffdaa6620e2c672f74443be4e3660ef5f
anames2323 <137616037+anames2323@users.noreply.github.com>
2026-06-22 23:22:50 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 token = '8762089094:AAGvv2yrQONhEauSJgwcL4l5GrLlK9JrK9c'  # Токен бота
+
 NICNAME_BOT = 'anonimmassagingbot'  # username бота без @
 
-# ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА СВОИ!
-CHANNEL_ID = -1004356247194  # ID вашего канала (с минусом!)
-CHANNEL_URL = 'https://t.me/anonimsoobsheniabro'  # Ссылка на канал
-CHANNEL_NAME = 'Наш канал'  # Название канала для отображения
+
+
+# ID ГЛАВНОГО АДМИНИСТРАТОРА (ВАШ ID)
+
+# Получить свой ID можно у бота @getmyid_bot
+
+MAIN_ADMIN_ID = 8795006636  # 👈 ЗАМЕНИТЕ НА СВОЙ ID!
